e396f313302ee8cf5eca554411aaecd4fbfd5178
Piotr Cz <czajkowskipp@gmail.com>
2020-01-18 18:13:23 +0100

    merge to master

--- src/main/java/inputhandling/CustomCSVReader.java
+++ src/main/java/inputhandling/CustomCSVReader.java
@@ -61,8 +61,8 @@ public class CustomCSVReader
 
     private void setSizes(Set<Record> set) {
         int size = set.size();
-        int testSize = (int) (size * 0.1);
-        this.trainSize = (int) (size * 0.45);
+        int testSize = (int) (size * 0.3);
+        this.trainSize = (int) (size * 0.5);
         this.pruneSize = size - this.trainSize - testSize;
     }
 
