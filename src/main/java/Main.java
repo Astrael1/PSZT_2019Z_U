e8f57771dd8b159dd28baf25a07d1fa3021f9199
Piotr Czajkowski <p.czajkowsk2@samsung.com>
2020-01-24 12:31:15 +0100

    gradle working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,35 +2,27 @@ import decisiontree.DecisionTreeBuilder;
 import decisiontree.Node;
 import inputhandling.CustomCSVReader;
 import inputhandling.Record;
-import org.apache.commons.cli.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class Main
 {
 
-    private static CommandLine parseArguments(String args[]) throws ParseException {
-        Options options = new Options();
-        options.addOption("l", true, "program learns from given source");
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(options, args);
-        return cmd;
-    }
     public static void main(String args[]) throws Exception
     {
-        CommandLine arguments = parseArguments(args);
-
         String csvFilePath;
-
-        if(arguments.hasOption("l"))
-        {
-            csvFilePath = arguments.getOptionValue("l");
+        List argsList = Arrays.asList(args);
+        if(argsList.contains("-l")) {
+        csvFilePath = (String) argsList.get(argsList.indexOf("-l") + 1);
         }
         else
         {
             csvFilePath = System.getProperty("user.dir") + "/src/main/resources/divorce.csv";
         }
+
+
 
         CustomCSVReader cr = new CustomCSVReader();
         Set<Record> dataSet = cr.read(csvFilePath);
